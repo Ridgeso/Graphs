25a3c10c6e5000b86eb95202715f57d8af648dae
kastre3381 <kacper.tracz3381@gmail.com>
2024-03-06 19:06:34 +0100

    drawing functions

--- draw/graph_drawer.py
+++ draw/graph_drawer.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+##### lab 1 excercide 2 #######
+
 def Draw_Graph(G, pos = None, color='skyblue', labels = True):
     nx.draw(G, pos = pos, with_labels = labels, node_color=color)
     plt.show()
