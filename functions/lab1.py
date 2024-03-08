7a5a0527e81932440b64cc9b5afe1acbebb9d2f0
Jakub Mikusek <j.mikusek01@gmail.com>
2024-03-08 15:00:15 +0100

    Added lab1_ex1 to functions module

--- functions/lab1.py
+++ functions/lab1.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import random
+import re
 
 def Erodos_Renyia_Edges(n,l):
     G = nx.Graph()
@@ -29,5 +30,101 @@ def Erodos_Renyia_Probability(n,l):
                 G.add_edge(int(i), int(j))
     return G
 
+def CountVertices(filename : str) :
+	vertCount = 0
+	with open(filename) as file :
+		line = file.readline()
+		while line != '' :
+			vertCount += 1
+			line = file.readline()
+	return vertCount
 
+def NeighborhoodListToMatrix(filename : str, outputName : str) :
+	vertCount = CountVertices(filename)
+	NeighborhoodMtx = [['0' for _ in range(vertCount)] for _ in range(vertCount)]
+	with open(filename) as file :
+		format = '[0-9]+[.][ [0-9]*]*\n'
+		line  = file.readline()
+		while line != '':
+			if re.fullmatch(format, line) != None:
+				vertices = re.findall('[0-9]+', line)
+				for i in range(len(NeighborhoodMtx)) :
+					if str(i) in vertices and str(i) != vertices[0]:
+						NeighborhoodMtx[eval(vertices[0])][i] = '1'
+			line  = file.readline()
+	with open(outputName, 'w') as outFile :
+		for i in range(vertCount) :
+			for j in range(vertCount) :
+				if j != vertCount - 1 :
+					outFile.write(NeighborhoodMtx[i][j] + ' ')
+				else :
+					outFile.write(NeighborhoodMtx[i][j] + '\n')
 
+def NeighborhoodMatrixToList(filename : str, outputName : str) :
+	NeighborhoodList = []
+	vertCount = CountVertices(filename)
+	with open(filename) as file :
+		line  = file.readline()
+		while line != '':
+			vertices = re.findall('[0-1]', line)
+			if len(vertices) == vertCount :
+				NeighborhoodList.append(str(len(NeighborhoodList)) + '.')
+				for i in range(len(vertices)) :
+					if vertices[i] == '1' :
+						NeighborhoodList[len(NeighborhoodList) - 1] += ' ' + str(i)
+			line  = file.readline()
+	with open(outputName, 'w') as outFile :
+		for row in NeighborhoodList :
+			outFile.write(row + '\n')
+
+def NeighborhoodMatrixToIncidence(filename : str, outputName : str) :
+	NeighborhoodMtx = []
+	vertCount = CountVertices(filename)
+	IncidenceMtx = []
+	with open(filename) as file :
+		line  = file.readline()
+		while line != '':
+			vertices = re.findall('[0-1]', line)
+			NeighborhoodMtx.append(vertices)
+			line  = file.readline()
+	for i in range(len(NeighborhoodMtx)) :
+		for j in range(i) :
+			if NeighborhoodMtx[i][j] == '1':
+				IncidenceMtx.append(['0' for _ in range(vertCount)])
+				IncidenceMtx[len(IncidenceMtx) - 1][i] = IncidenceMtx[len(IncidenceMtx) - 1][j] = '1'
+	with open(outputName, 'w') as outFile :
+		for j in range(len(IncidenceMtx[0])) :
+			for i in range(len(IncidenceMtx)) :
+				if i != len(IncidenceMtx) - 1 :
+					outFile.write(IncidenceMtx[i][j] + ' ')
+				else :
+					outFile.write(IncidenceMtx[i][j] + '\n')
+
+def IncidenceToNeighborhoodMatrix(filename : str, outputName : str) :
+	IncidenceMtx = []
+	vertCount = CountVertices(filename)
+	NeighborhoodMtx = [['0' for _ in range(vertCount)] for _ in range(vertCount)]
+	with open(filename) as file :
+		line  = file.readline()
+		while line != '':
+			vertices = re.findall('[0-1]', line)
+			IncidenceMtx.append(vertices)
+			line  = file.readline()
+	for j in range(len(IncidenceMtx[0])) :
+		ids = [0, 0]
+		k = 0
+		for i in range(len(IncidenceMtx)) :
+			if IncidenceMtx[i][j] == '1':
+				ids[k] = i
+				k = (k + 1) % 2
+		NeighborhoodMtx[ids[1]][ids[0]] = '1'
+	for i in range(len(NeighborhoodMtx)) :
+		for j in range(i) :
+			NeighborhoodMtx[j][i] = NeighborhoodMtx[i][j]
+	with open(outputName, 'w') as outFile :
+		for i in range(vertCount) :
+			for j in range(vertCount) :
+				if j != vertCount - 1 :
+					outFile.write(NeighborhoodMtx[i][j] + ' ')
+				else :
+					outFile.write(NeighborhoodMtx[i][j] + '\n')
--- testing.py
+++ testing.py
@@ -4,3 +4,7 @@ from functions import lab1
 drw.Draw_Graph_Circular(lab1.Erodos_Renyia_Probability(10, 0.2))
 drw.Draw_Graph_Circular(lab1.Erodos_Renyia_Edges(10, 14))
 
+lab1.NeighborhoodMatrixToList('test.txt', 'NeighborhoodList.txt')
+lab1.NeighborhoodListToMatrix('NeighborhoodList.txt', 'NeighborhoodMtx.txt')
+lab1.NeighborhoodMatrixToIncidence('NeighborhoodMtx.txt', 'IncidenceMtx.txt')
+lab1.IncidenceToNeighborhoodMatrix('IncidenceMtx.txt', 'NeighborhoodMtx2.txt')
