cf972ec6f0e782633d70241d757b5c23af1b6c1a
Jakub Mikusek <j.mikusek01@gmail.com>
2024-03-09 13:58:58 +0100

    Added Lab2 ex1, ex2

---
+++ functions/lab2.py
@@ -0,0 +1,61 @@
+import networkx as nx
+import random
+
+def CheckZerosInSeq(A : list) :
+	allZeros = True
+	for val in A :
+		if val != 0 :
+			allZeros = False
+	if allZeros :
+		return allZeros
+
+def GraphicSeqCheck(A : list) :
+	while True :
+		A = sorted(A, reverse = True)
+		if CheckZerosInSeq(A) :
+			return True
+		if A[0] >= len(A) or A[len(A) - 1] < 0 :
+			return False
+		for i in range(1, A[0] + 1) :
+			A[i] -= 1
+		A[0] = 0
+
+def GraphFromGraphicSeq(A : list) :
+	G = nx.Graph()
+	if not GraphicSeqCheck(A) :
+		return G
+	for i in range(len(A)) :
+		G.add_node(i)
+	A = sorted(A, reverse = True)
+	dictA = {i : A[i] for i in range(len(A))}
+	while not CheckZerosInSeq(A) :
+		A = sorted(A, reverse = True)
+		dictA = dict(sorted(dictA.items(), key=lambda item: item[1], reverse = True))
+		for i in range(1, A[0] + 1) :
+			G.add_edge(list(dict.keys(dictA))[0], list(dict.keys(dictA))[i])
+			A[i] -= 1
+			dictA[list(dict.keys(dictA))[i]] -= 1
+		A[0] = 0
+		dictA[list(dict.keys(dictA))[0]] = 0
+	return G
+
+def RandomizeEdges(G : nx.Graph, numberOfRandomizes : int) :
+	print(G.edges)
+	def FindEdges(G) :
+		edge1 = random.randrange(0, len(G.edges))
+		edge2 = random.randrange(0, len(G.edges))
+		while edge2 == edge1 :
+			edge2 = random.randrange(0, len(G.edges))
+		return list(G.edges)[edge1], list(G.edges)[edge2]
+	
+	for i in range(numberOfRandomizes) :
+		newEdges = FindEdges(G)
+		tol = 10
+		while G.has_edge(newEdges[0][0], newEdges[1][1]) or G.has_edge(newEdges[0][1], newEdges[1][0]) or newEdges[0][1] == newEdges[1][0] or newEdges[0][0] == newEdges[1][1]:
+			newEdges = FindEdges(G)
+		G.add_edge(newEdges[0][0], newEdges[1][1]) 
+		G.add_edge(newEdges[0][1], newEdges[1][0])
+		G.remove_edge(newEdges[0][0], newEdges[0][1]) 
+		G.remove_edge(newEdges[1][0], newEdges[1][1]) 
+	print(G.edges)
+	return G
--- testing.py
+++ testing.py
@@ -1,10 +1,15 @@
 from draw import graph_drawer as drw
 from functions import lab1
+from functions import lab2
 
-drw.Draw_Graph_Circular(lab1.Erodos_Renyia_Probability(10, 0.2))
-drw.Draw_Graph_Circular(lab1.Erodos_Renyia_Edges(10, 14))
-
-lab1.NeighborhoodMatrixToList('test.txt', 'NeighborhoodList.txt')
-lab1.NeighborhoodListToMatrix('NeighborhoodList.txt', 'NeighborhoodMtx.txt')
-lab1.NeighborhoodMatrixToIncidence('NeighborhoodMtx.txt', 'IncidenceMtx.txt')
-lab1.IncidenceToNeighborhoodMatrix('IncidenceMtx.txt', 'NeighborhoodMtx2.txt')
+# drw.Draw_Graph_Circular(lab1.Erodos_Renyia_Probability(10, 0.2))
+# drw.Draw_Graph_Circular(lab1.Erodos_Renyia_Edges(10, 14))
+############################
+# lab1.NeighborhoodMatrixToList('test.txt', 'NeighborhoodList.txt')
+# lab1.NeighborhoodListToMatrix('NeighborhoodList.txt', 'NeighborhoodMtx.txt')
+# lab1.NeighborhoodMatrixToIncidence('NeighborhoodMtx.txt', 'IncidenceMtx.txt')
+# lab1.IncidenceToNeighborhoodMatrix('IncidenceMtx.txt', 'NeighborhoodMtx2.txt')
+#############################
+print(lab2.GraphicSeqCheck([4, 3, 3, 2, 2, 1, 1]))
+drw.Draw_Graph_Circular(lab2.GraphFromGraphicSeq([1, 3, 2, 3, 2, 4, 1]))
+drw.Draw_Graph_Circular(lab2.RandomizeEdges(lab2.GraphFromGraphicSeq([1, 3, 2, 3, 2, 4, 1]), 5))
