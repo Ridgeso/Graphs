d98b3eff32133c6f9d961c957e16bb5fecacbbce
ZEN4est <mi.piwowarczyk@gmail.com>
2024-03-12 22:39:42 +0100

    lab 2 zad 5

--- draw/graph_drawer.py
+++ draw/graph_drawer.py
@@ -1,19 +1,21 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
+
 
 ##### lab 1 excercide 2 #######
 
 def Draw_Graph(G, pos = None, color='skyblue', labels = True):
     nx.draw(G, pos = pos, with_labels = labels, node_color=color)
-    plt.show()
+    plt.savefig(os.path.abspath(os.path.dirname(__file__)))
 
 def Draw_Graph_Circular(G, color='skyblue', labels = True):
     nx.draw_circular(G, with_labels = labels, node_color=color)
-    plt.show()
+    plt.savefig(os.path.abspath(os.path.dirname(__file__)))
 
 def Draw_Graph_Planar(G, color='skyblue', labels = True):
     nx.draw_planar(G, with_labels = labels, node_color=color)
-    plt.show()
+    plt.savefig(os.path.abspath(os.path.dirname(__file__)))
 
 def Draw_Graph_Circular_Multicolor(G : nx.Graph, colorToNodes, colors, labels = True):
     nodesToColor = {}
@@ -22,4 +24,4 @@ def Draw_Graph_Circular_Multicolor(G : nx.Graph, colorToNodes, colors, labels = 
     nodesToColor = dict(sorted(nodesToColor.items()))
     nodeColors = [colors[nodesToColor[node]] for node in nodesToColor]
     nx.draw_circular(G, with_labels = labels, node_color=nodeColors)
-    plt.show()
+    plt.savefig(os.path.abspath(os.path.dirname(__file__)))
