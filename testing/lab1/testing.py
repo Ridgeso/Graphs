8df0ae77b8e3fcd831be55256d675d0646033cd3
Jakub Mikusek <j.mikusek01@gmail.com>
2024-04-18 09:39:26 +0200

    added testing for drawing from files

--- testing/lab1/testing.py
+++ testing/lab1/testing.py
@@ -6,10 +6,16 @@ from draw import graph_drawer as drw
 from functions import lab1
 
 #-------------------EXERCISE 1----------------------------#
-lab1.NeighborhoodMatrixToList('test.txt', 'NeighborhoodList.txt')
-lab1.NeighborhoodListToMatrix('NeighborhoodList.txt', 'NeighborhoodMtx.txt')
-lab1.NeighborhoodMatrixToIncidence('NeighborhoodMtx.txt', 'IncidenceMtx.txt')
-lab1.IncidenceToNeighborhoodMatrix('IncidenceMtx.txt', 'NeighborhoodMtx2.txt')
+lab1.NeighborhoodMatrixToList('test.txt', 'NList.txt')
+lab1.NeighborhoodListToMatrix('NList.txt', 'NMtx.txt')
+lab1.NeighborhoodMatrixToIncidence('NMtx.txt', 'IMtx.txt')
+lab1.IncidenceToNeighborhoodMatrix('IMtx.txt', 'NMtx2.txt')
+lab1.IncidenceToNeighborhoodList('IMtx.txt', 'NList2.txt')
+lab1.NeighborhoodListToIncidence('NList.txt', 'IMtx2.txt')
+
+drw.DrawGraphCircular(lab1.GetGraphFromNeighMtx('test.txt'), name = 'Graf z macierzy sasiedztwa')
+drw.DrawGraphCircular(lab1.GetGraphFromList('NList.txt'), name = 'Graf z listy sasiedztwa')
+drw.DrawGraphCircular(lab1.GetGraphFromIncidMtx('IMtx.txt'), name = 'Graf z macierzy incydencji')
 #----------------------------------------------------------#
 
 
